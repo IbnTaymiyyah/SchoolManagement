be7213aa4c38e6f6a1528a7a7108b3d86cba5203
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 06:47:09 +0200

    Add "Parent Class Model "

---
+++ SchoolManagement/Models/Parent.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolManagement.Models
+{
+    public class Parent
+    {
+
+        public int Id { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        
+
+    }
+}
