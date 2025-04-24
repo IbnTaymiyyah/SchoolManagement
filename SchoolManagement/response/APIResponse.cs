a58b89a459c7ae41718cf0df585208aaf8848e43
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:22:47 +0200

    Add : "Custom API Response"

---
+++ SchoolManagement/response/APIResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolManagement.response
+{
+    public class APIResponse
+    {
+        public string StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public List<string> Error { get; set; }
+        public object Data { get; set; }
+
+    }
+}
