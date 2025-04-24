c0f0ec6c9c0b69f4a9b8b4439a5cad5eadc1a2a9
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 06:48:41 +0200

    Add  "Teacher Class Model"

---
+++ SchoolManagement/Models/Teacher.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolManagement.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+    }
+}
