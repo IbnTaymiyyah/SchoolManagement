34633f9555a49c0c3a1dffc5576d6c455740eb71
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 08:03:53 +0200

    Add : "Subject Update DTO"

---
+++ SchoolManagement/DTO/SubjectUpdateDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class SubjectUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        
+        public string Description { get; set; } = string.Empty;
+
+        
+    }
+}
