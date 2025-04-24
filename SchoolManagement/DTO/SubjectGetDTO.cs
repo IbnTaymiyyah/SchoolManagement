398ebe1abbced3a4941c00915fac874d9d66bcbf
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 08:03:40 +0200

    Add : "Subject Get DTO"

---
+++ SchoolManagement/DTO/SubjectGetDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class SubjectGetDTO
+    {
+        
+        public int Id { get; set; }
+
+        
+        public string Name { get; set; } 
+
+
+        public string Description { get; set; } 
+    }
+}
