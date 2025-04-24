50e6909f596c96dfd8ad1db43f4fbece0a13d153
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:08:59 +0200

    Add : "Student Class Model"

---
+++ SchoolManagement/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolManagement.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender {  get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+
+        // Foreign Keys
+
+        [ForeignKey("Parent")]
+        public int ParentId { get; set; }
+        public Parent Parent { get; set; } 
+        
+        
+        [ForeignKey("Class")]
+        public int ClassId { get; set; }
+        public Class Class { get; set; } 
+
+    }
+}
