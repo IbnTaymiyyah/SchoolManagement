89dc8690d27275b417c4553831f4560546e0f318
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:09:31 +0200

    Add : "Classes Class Model"

---
+++ SchoolManagement/Models/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolManagement.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string AcademicYear { get; set; } // Should Be like 2024-2025
+    }
+}
