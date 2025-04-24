961828a788fe610d4113ab230956273993736c07
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:43:37 +0200

    Add : "Student Get DTO"

---
+++ SchoolManagement/DTO/StudentGetDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class StudentGetDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Gender { get; set; } = string.Empty;
+
+
+        public string Address { get; set; } = string.Empty;
+
+
+        public int PhoneNumber { get; set; }
+    }
+}
