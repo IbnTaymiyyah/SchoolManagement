5d24e83e1de248a9bc157dad966873775f34d0eb
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 08:08:44 +0200

    Add : "Class Update DTO"

---
+++ SchoolManagement/DTO/ClassUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class ClassUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string AcadmicYear { get; set; } = string.Empty;
+    }
+}
