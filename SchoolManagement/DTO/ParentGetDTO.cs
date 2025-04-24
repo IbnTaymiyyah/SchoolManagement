6d635d49db4c69dd118b0fe70442ace1393d96e2
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:48:58 +0200

    Add : "Parent GET DTO"

---
+++ SchoolManagement/DTO/ParentGetDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class ParentGetDTO
+    {
+        
+        public int Id { get; set; }
+
+        
+        public string Name { get; set; } = string.Empty;
+
+        
+        public string Email { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public int PhoneNumber { get; set; }
+    }
+}
