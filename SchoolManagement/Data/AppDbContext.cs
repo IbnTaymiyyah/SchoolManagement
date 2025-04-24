4d773da120ec46abd7c1a4220a4ca37e3d132307
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:14:00 +0200

    Add : "DB Context Class"

---
+++ SchoolManagement/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManagement.Models;
+
+namespace SchoolManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        
+    }
+}
