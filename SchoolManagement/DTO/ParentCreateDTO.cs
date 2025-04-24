df497424553604c820fb660e806389fdf2a2c429
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:49:11 +0200

    Add : "Parent Create DTO"

---
+++ SchoolManagement/DTO/ParentCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class ParentCreateDTO
+    {
+      
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+
+        public string Address { get; set; } = string.Empty;
+
+
+        public int PhoneNumber { get; set; }
+    }
+}
