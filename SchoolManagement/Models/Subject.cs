abe7fd52f387fa64e664937629b1cf6c1866d452
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:09:59 +0200

    Add : "Subject Class Model"

---
+++ SchoolManagement/Models/Subject.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolManagement.Models
+{
+    public class Subject
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Foreign Keys
+
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        
+        [ForeignKey("Teacher")]
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
