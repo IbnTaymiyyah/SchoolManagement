a7a43609113124b3726439d638e3f896b98ee9e7
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:48:05 +0200

    Fix : "Student Get DTO"

--- SchoolManagement/DTO/StudentGetDTO.cs
+++ SchoolManagement/DTO/StudentGetDTO.cs
@@ -4,13 +4,13 @@ namespace SchoolManagement.DTO
 {
     public class StudentGetDTO
     {
-        [Required]
+    
         public int Id { get; set; }
 
-        [Required]
+       
         public string Name { get; set; } = string.Empty;
 
-        [Required]
+    
         public string Gender { get; set; } = string.Empty;
 
 
