16197e20f4e09ad4b24827c618f466144da91304
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 08:03:24 +0200

    Add : "Subject Create DTO"

---
+++ SchoolManagement/DTO/SubjectCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class SubjectCreateDTO
+    {
+       
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
