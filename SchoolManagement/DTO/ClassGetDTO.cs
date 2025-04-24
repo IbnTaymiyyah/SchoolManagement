b113fed9ad5e3d8a28e96e2b6b2b265f05e34889
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 08:08:26 +0200

    Add : "Class GET DTO"

---
+++ SchoolManagement/DTO/ClassGetDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class ClassGetDTO
+    {
+      
+        public int Id { get; set; }
+
+        
+        public string Name { get; set; }
+
+     
+        public string AcadmicYear { get; set; } 
+    }
+}
