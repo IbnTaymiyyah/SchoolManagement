52f556b8b34daebc959a66a0877894d66b6027be
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:48:44 +0200

    Add : "Parent Update DTO"

---
+++ SchoolManagement/DTO/ParentUpdateDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class ParentUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public int PhoneNumber { get; set; }
+    }
+}
