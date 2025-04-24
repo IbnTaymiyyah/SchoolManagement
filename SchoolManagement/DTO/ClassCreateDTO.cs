7132c587c4b4719cdf8e5294952cb6ce448fd189
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 08:08:11 +0200

    Add : "Class Create DTO"

---
+++ SchoolManagement/DTO/ClassCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class ClassCreateDTO
+    {
+    
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string AcadmicYear { get; set; } = string.Empty;
+    }
+}
