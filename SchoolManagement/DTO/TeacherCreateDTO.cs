d5f950e7e001b9aebb35a32bf5275cc502c9f62a
Adham Khaled <115818785+IbnTaymiyyah@users.noreply.github.com>
2025-04-24 07:55:53 +0200

    Add : "Teacher Create DTO"

---
+++ SchoolManagement/DTO/TeacherCreateDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.DTO
+{
+    public class TeacherCreateDTO
+    {
+
+
+      
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Gender { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+
+        public string Address { get; set; } = string.Empty;
+
+
+
+    }
+}
